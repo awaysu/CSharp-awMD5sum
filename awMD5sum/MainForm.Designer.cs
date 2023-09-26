7329e46cfaec3f8ca51c4ec8c061f6add83bb4cd
jason_su <jason_su@arcadyan.com>
2023-09-26 10:29:50 +0800

    add

---
+++ awMD5sum/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jason_su
+ * Date: 11/19/2021
+ * Time: AM 11:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace awMD5sum
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button1.Location = new System.Drawing.Point(29, 26);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(450, 54);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Select File";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(29, 114);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(936, 484);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.WordWrap = false;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button2.Location = new System.Drawing.Point(515, 26);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(450, 54);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Select Folder";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button3.Location = new System.Drawing.Point(515, 615);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(450, 54);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Save to md5.txt";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1002, 744);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Icon = global::awMD5sum.Resource1.favicon;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "awMD5sum by Awaysu";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ awMD5sum/MainForm.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jason_su
+ * Date: 11/19/2021
+ * Time: AM 11:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Resources;
+
+
+namespace awMD5sum
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string spath = "";
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		public string checkMD5(string filename)
+		{
+			String ret ="error	" + filename;
+		    using (var md5 = MD5.Create())
+		    {
+		        using (var stream = File.OpenRead(filename))
+		        {
+		            var hash = md5.ComputeHash(stream);
+		            ret = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant() + "	" + Path.GetFileName(filename);
+		            return ret;
+		        }
+		    }
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			OpenFileDialog choofdlog = new OpenFileDialog();
+			choofdlog.Filter = "All Files (*.*)|*.*";
+			choofdlog.FilterIndex = 1;
+			//choofdlog.Multiselect = true;
+			
+			if (choofdlog.ShowDialog() == DialogResult.OK)    
+			{     
+				string sFileName = choofdlog.FileName; 
+				//string[] arrAllFiles = choofdlog.FileNames; //used when Multiselect = true  
+				
+				this.spath = Path.GetFullPath (sFileName);
+				textBox1.Text = checkMD5(sFileName);				
+			}
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			fbd.Description = "Custom Description"; 
+			
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				string list = "";
+				this.spath = fbd.SelectedPath;
+				
+			    string [] fileEntries = Directory.GetFiles(fbd.SelectedPath);
+			    for (int i = 0; i!= fileEntries.Length; i++)
+			    {
+			    	list += checkMD5(fileEntries[i]) + "\r\n";
+			    }
+			    textBox1.Text = list;
+			}
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+	
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			if (this.spath.Length > 0 && textBox1.Text.Length > 0)
+			{
+				try
+				{
+					File.WriteAllText(this.spath + "\\md5.txt", textBox1.Text);
+					MessageBox.Show("Done!!", "");
+				}
+				catch{
+					MessageBox.Show("Write md5.txt failed!!", "Warning");
+				}			
+			}
+			else
+				MessageBox.Show("There is no content!!", "Warning");
+		}
+	}
+}
